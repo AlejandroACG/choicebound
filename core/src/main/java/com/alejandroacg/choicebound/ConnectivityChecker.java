bb292a3629c54aa5d5c41a28797b9c09d3b36552
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2025-05-20 01:39:52 +0200

    update checkconnectivity and spanish integration

--- core/src/main/java/com/alejandroacg/choicebound/ConnectivityChecker.java
+++ core/src/main/java/com/alejandroacg/choicebound/ConnectivityChecker.java
@@ -1,9 +1,20 @@
 package com.alejandroacg.choicebound;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.alejandroacg.choicebound.interfaces.PlatformBridge;
+import com.badlogic.gdx.utils.Align;
 
 public class ConnectivityChecker {
     private final PlatformBridge platformBridge;
@@ -17,16 +28,46 @@ public class ConnectivityChecker {
     public boolean checkConnectivity(Stage stage) {
         boolean isConnected = platformBridge.hasInternetConnection();
         if (!isConnected) {
-            Dialog dialog = new Dialog("", skin) {
+            // Crear un Group para el mensaje de error
+            final Group overlayGroup = new Group();
+            overlayGroup.setSize(stage.getWidth(), stage.getHeight());
+
+            // Crear un Pixmap con el color negro semitransparente
+            Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+            Color color = skin.getColor("black_semi_transparent");
+            pixmap.setColor(color);
+            pixmap.fill();
+
+            // Crear un Texture y un Drawable
+            Texture texture = new Texture(pixmap);
+            TextureRegionDrawable drawable = new TextureRegionDrawable(texture);
+
+            // Fondo negro semitransparente
+            Image background = new Image(drawable);
+            background.setSize(stage.getWidth(), stage.getHeight());
+            overlayGroup.addActor(background);
+
+            // Mensaje centrado
+            Label message = new Label(GameConfig.getString("no_internet_connection"), skin, "error_message");
+            message.setWrap(true);
+            message.setWidth(stage.getWidth() * 0.8f); // 80% del ancho de la pantalla
+            message.setAlignment(Align.center); // Centra el texto dentro del Label
+            message.setPosition((stage.getWidth() - message.getWidth()) / 2, stage.getHeight() / 2);
+            overlayGroup.addActor(message);
+
+            // Hacer el Group modal y eliminarlo al hacer clic
+            overlayGroup.setTouchable(Touchable.enabled);
+            overlayGroup.addListener(new ClickListener() {
                 @Override
-                protected void result(Object object) {
-                    remove();
+                public void clicked(InputEvent event, float x, float y) {
+                    overlayGroup.remove();
                 }
-            };
-            dialog.text(GameConfig.getString("no_internet_connection"));
-            dialog.button("OK");
-            dialog.setModal(true);
-            dialog.show(stage);
+            });
+
+            stage.addActor(overlayGroup);
+
+            // Liberar recursos
+            pixmap.dispose();
         }
         return isConnected;
     }
--- core/src/main/java/com/alejandroacg/choicebound/GameConfig.java
+++ core/src/main/java/com/alejandroacg/choicebound/GameConfig.java
@@ -3,13 +3,15 @@ package com.alejandroacg.choicebound;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.I18NBundle;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 public class GameConfig {
     private static I18NBundle strings;
 
     public static void initialize() {
-        strings = I18NBundle.createBundle(Gdx.files.internal("i18n/strings"), new Locale("es"));
+        I18NBundle.setSimpleFormatter(true);
+        strings = I18NBundle.createBundle(Gdx.files.internal("i18n/strings"), new Locale("es"), StandardCharsets.UTF_8.name());
     }
 
     public static String getString(String key) {
